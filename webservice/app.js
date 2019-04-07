4a6526d4f2e61e1d220fa5fefa0a25db347cda10
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-04-07 18:36:18 +0800

    调整查询算法

--- webservice/app.js
+++ webservice/app.js
@@ -18,18 +18,51 @@ app.post('/', async (req, res) => {
     try{
         ps.input('cardnum', sql.VarChar(9))
         ps.input('name', sql.NVarChar(50))
-        await ps.prepare("SELECT cardnum, name, location, dateStr FROM CardRecord WHERE cardnum=@cardnum AND name=@name GROUP BY cardnum, name, location, dateStr")
+        await ps.prepare("SELECT cardnum, name, location, dateStr, timestamp FROM CardRecord WHERE cardnum=@cardnum AND name=@name GROUP BY cardnum, name, location, dateStr, timestamp ORDER BY timestamp")
         let rawResult = (await ps.execute({cardnum, name})).recordset
-        let result = []
+        let result1 = []
+        // result1 中是按照10分钟去重的记录
         rawResult.forEach( r => {
-            try{
-                let lectureHistory = lectureMap[r.dateStr][r.location]
-                r.lectureTitle = lectureHistory.name
-                r.lectureUrl = lectureHistory.url
-                result.push(r)
-            } catch(e) {
-                result.push(r)
+            if(result1.length === 0){
+                result1.push(r)
+            } else {
+                if(r.timestamp - result1[result1.length - 1].timestamp > 10 * 60 * 100) {
+                    result1.push(r)
+                }
             }
+        })
+        let result2 = {}
+        result1.forEach ( r => {
+            if(!result2[r.dateStr]) {
+                result2[r.dateStr] = {}
+            }
+            if(!result2[r.dateStr][r.location]){
+                result2[r.dateStr][r.location] = []
+            }
+            if(lectureMap[r.dateStr] && lectureMap[r.dateStr][r.location]) {
+                // 如果存在当日当地讲座具体记录
+                if(result2[r.dateStr][r.location].length < lectureMap[r.dateStr]
+                [r.location].length){
+                    // 并且当日当地的讲座记录多于当前查询人当日当地的打卡记录
+                    let index = result2[r.dateStr][r.location].length
+                    let history = lectureMap[r.dateStr][r.location][index]
+                    r.lectureTitle = history.name
+                    r.lectureUrl = history.url
+                    result2[r.dateStr][r.location].push(r)
+                }
+            } else {
+                // 没有当日记录则只记录一次有效记录
+                if(result2[r.dateStr][r.location].length === 0){
+                    result2[r.dateStr][r.location].push(r)
+                }
+            }
+        })
+        // 收集所有有效记录
+        let result = []
+        Object.keys(result2).forEach( dateStr => {
+            Object.keys(result2[dateStr]).forEach( location => {
+                result = result.concat(result2[dateStr][location])
+            })
         })
         result.sort((a,b)=>{
             return (+moment(a.dateStr, 'YYYY-MM-DD'))-(+moment(b.dateStr, 'YYYY-MM-DD'))
@@ -59,9 +92,9 @@ app.post('/', async (req, res) => {
             lectureMap[k.dateStr] = {}
         }
         if(!lectureMap[k.dateStr][k.location]){
-            lectureMap[k.dateStr][k.location] = {}
+            lectureMap[k.dateStr][k.location] = []
         }
-        lectureMap[k.dateStr][k.location] = k
+        lectureMap[k.dateStr][k.location].push(k)
     })
     app.listen(3000, ()=>{console.log('[+]服务已启动')})
 })();
