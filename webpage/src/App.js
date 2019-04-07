17480e1b3fd4234b34575f48c9b9e16030e3356e
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-04-08 00:42:32 +0800

    验证码的获取

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      cardnum:'', name:'', token:'', captcha:'', captchaSVG:''
+    }
+    axios.get('https://lecture.myseu.cn/api/captcha').then(res => {
+      this.setState({token:res.data.token, captchaSVG:res.data.captcha})
+    })
+    
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       <div dangerouslySetInnerHTML={{__html: this.state.captchaSVG}} />
       </div>
     );
   }
